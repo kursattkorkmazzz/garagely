9be700d06e480e188a70ddff593ce55d0de49af6
Kursat KORKMAZ <kursat.korkmaz123@hotmail.com>
2026-04-17 17:38:58 +0300

    feat: Add unistyles theme configuration and remove unused theme hooks

--- babel.config.js
+++ babel.config.js
@@ -14,6 +14,16 @@ module.exports = function (api) {
         },
       ],
       "react-native-worklets/plugin",
+      [
+        "react-native-unistyles/plugin",
+        {
+          // pass root folder of your application
+          // all files under this folder will be processed by the Babel plugin
+          // if you need to include more folders, or customize discovery process
+          // check available babel options
+          root: "./",
+        },
+      ],
     ],
   };
 };
--- theme/index.ts
+++ theme/index.ts
@@ -1,7 +1,3 @@
-export * from "@/theme/hooks/use-theme";
-export * from "@/theme/hooks/use-theme-styles";
-export * from "@/theme/hooks/use-theme-sva";
-export * from "@/theme/theme-service";
 export * from "@/theme/tokens/colors";
 export * from "@/theme/tokens/radius";
 export * from "@/theme/tokens/spacing";
